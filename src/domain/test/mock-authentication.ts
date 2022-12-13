75bd9e83ee8ba69e997c723a5fe527b7c5e16c71
Gustavo Cotta <gmcotta34@gmail.com>
2022-12-13 20:23:10 -0300

    refactor: change imports to use tsconfig's paths

--- src/domain/test/mock-authentication.ts
+++ src/domain/test/mock-authentication.ts
@@ -1,5 +1,6 @@
-import { AuthenticationParams } from 'domain/usecases/authentication'
 import { faker } from '@faker-js/faker'
+
+import { AuthenticationParams } from '@/domain/usecases/authentication'
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
