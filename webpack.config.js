d4396601ec4ac7e75864776ccc1ae83998b7d12a
Gustavo Cotta <gmcotta34@gmail.com>
2022-12-15 21:44:21 -0300

    chore: config webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  devServer: {
+    static: './public',
+    devMiddleware: {
+      writeToDisk: true
+    },
+    historyApiFallback: {
+      rewrites: [
+        {
+          from: /./,
+          to: '/index.html'
+        }
+      ]
+    }
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+}
