966cc9b49da48d89a6b533a830e86f12e55ac25b
Gustavo Cotta <gmcotta34@gmail.com>
2022-12-17 10:10:11 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -2,21 +2,29 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Login from './login'
 
+// type SutTypes = {
+
+// }
+
+const makeSut = (): void => {
+  render(<Login />)
+}
+
 describe('<Login />', () => {
   it('Should not render spinner and error message on start', () => {
-    render(<Login />)
+    makeSut()
     const errorWrap = screen.getByLabelText('Status do formulário')
     expect(errorWrap.childElementCount).toBe(0)
   })
 
   it('Should render button disabled on start', () => {
-    render(<Login />)
+    makeSut()
     const submitButton = screen.getByRole<HTMLButtonElement>('button', { name: /entrar/i })
     expect(submitButton.disabled).toBe(true)
   })
 
   it('Should render input status errors on start', () => {
-    render(<Login />)
+    makeSut()
     // screen.logTestingPlaygroundURL()
     const emailErrorStatus = screen.getByLabelText(/email-error-status/i)
     const passwordErrorStatus = screen.getByLabelText(/password-error-status/i)
