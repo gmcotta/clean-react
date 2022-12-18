63919aea5551ca1709e804d5ffc3a12660ede649
Gustavo Cotta <gmcotta34@gmail.com>
2022-12-18 11:00:30 -0300

    feat: ensure EmailValidation returns null if email is valid

--- src/presentation/validation/validators/email/email-validation.ts
+++ src/presentation/validation/validators/email/email-validation.ts
@@ -4,6 +4,7 @@ import { InvalidFieldError } from '@/presentation/validation/errors'
 export class EmailValidation implements FieldValidation {
   constructor (readonly fieldName: string) {}
   validate (value: string): InvalidFieldError {
-    return new InvalidFieldError()
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return emailRegex.test(value) ? null : new InvalidFieldError()
   }
 }
