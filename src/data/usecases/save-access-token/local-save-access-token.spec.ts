65c7819ae16fc634c81427afcd02c1d1ad00cfee
Gustavo Cotta <gmcotta34@gmail.com>
2022-12-25 11:43:58 -0300

    test: ensure LocalSaveAccessToken calls SetStorage with correct value

---
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -0,0 +1,28 @@
+import { faker } from '@faker-js/faker'
+
+import { SetStorageSpy } from '@/data/test'
+import { LocalSaveAccessToken } from './local-save-access-token'
+
+type SutTypes = {
+  sut: LocalSaveAccessToken
+  setStorageSpy: SetStorageSpy
+}
+
+const makeSut = (): SutTypes => {
+  const setStorageSpy = new SetStorageSpy()
+  const sut = new LocalSaveAccessToken(setStorageSpy)
+  return {
+    sut,
+    setStorageSpy
+  }
+}
+
+describe('LocalSaveAccessToken', () => {
+  it('Should call setStorage with correct value', async () => {
+    const { sut, setStorageSpy } = makeSut()
+    const accessToken = faker.datatype.uuid()
+    await sut.save(accessToken)
+    expect(setStorageSpy.key).toBe('accessToken')
+    expect(setStorageSpy.value).toBe(accessToken)
+  })
+})
