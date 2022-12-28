9040e7c8cce33ff75d58e7e27e01180a86435125
Gustavo Cotta <gmcotta34@gmail.com>
2022-12-28 10:02:32 -0300

    chore: add and config cypress

---
+++ cypress.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: "http://localhost:8080",
+    supportFolder: "src/main/test/cypress/support",
+    supportFile: "src/main/test/cypress/support/e2e.ts",
+    specPattern: "src/main/test/cypress/integration/**/*.cy.{js,jsx,ts,tsx}"
+  },
+  fixturesFolder: "src/main/test/cypress/fixtures",
+  screenshotsFolder: "src/main/test/cypress/screenshots",
+  videosFolder: "src/main/test/cypress/videos"
+});
