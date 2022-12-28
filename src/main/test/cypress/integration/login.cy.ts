874d2097cf7ee2470022c35b28abcc82cbec4d16
Gustavo Cotta <gmcotta34@gmail.com>
2022-12-28 10:45:48 -0300

    test: ensure Login begin with correct initial state

--- src/main/test/cypress/integration/login.cy.ts
+++ src/main/test/cypress/integration/login.cy.ts
@@ -1,7 +1,21 @@
 /// <reference types="cypress" />
 
 describe('Login', () => {
-  it('Should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+
+  it('Should load with correct initial state', () => {
+    cy.getByAriaLabel('email-error-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('have.text', '🔴')
+    cy.getByAriaLabel('password-error-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('have.text', '🔴')
+    cy.get('button[type="submit"]')
+      .should('be.disabled')
+      .should('have.text', 'Entrar')
+    cy.getByAriaLabel('form-status').children()
+      .should('have.length', 0)
   })
 })
