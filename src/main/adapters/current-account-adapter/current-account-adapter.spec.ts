3191dc4f6e4984e1004c4cd3e0dd3a56a39601bc
Gustavo Cotta <gmcotta34@gmail.com>
2023-01-05 21:19:27 -0300

    test: add getCurrentAccountAdapter

--- src/main/adapters/current-account-adapter/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter/current-account-adapter.spec.ts
@@ -2,21 +2,34 @@ import { UnexpectedError } from '@/domain/errors'
 import { mockAccountModel } from '@/domain/test'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
 
-import { setCurrentAccountAdapter } from './current-account-adapter'
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from './current-account-adapter'
 
 jest.mock('@/infra/cache/local-storage-adapter')
 
 describe('CurrentAccountAdapter', () => {
-  it('Should call LocalStorageAdapter with correct values', () => {
-    const account = mockAccountModel()
-    const localStorageAdapterSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
-    setCurrentAccountAdapter(account)
-    expect(localStorageAdapterSpy).toHaveBeenCalledWith('account', account)
+  describe('.set', () => {
+    it('Should call LocalStorageAdapter with correct values', () => {
+      const account = mockAccountModel()
+      const localStorageAdapterSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
+      setCurrentAccountAdapter(account)
+      expect(localStorageAdapterSpy).toHaveBeenCalledWith('account', account)
+    })
+
+    it('Should throw UnexpectedError', () => {
+      expect(() => {
+        setCurrentAccountAdapter(undefined)
+      }).toThrow(new UnexpectedError())
+    })
   })
 
-  it('Should throw UnexpectedError', () => {
-    expect(() => {
-      setCurrentAccountAdapter(undefined)
-    }).toThrow(new UnexpectedError())
+  describe('.get', () => {
+    it('Should call LocalStorageAdapter.get with correct values', () => {
+      const accountMock = mockAccountModel()
+      const localStorageAdapterSpy = jest.spyOn(LocalStorageAdapter.prototype, 'get')
+        .mockReturnValueOnce(accountMock)
+      const account = getCurrentAccountAdapter()
+      expect(localStorageAdapterSpy).toHaveBeenCalledWith('account')
+      expect(accountMock).toStrictEqual(account)
+    })
   })
 })
