2a2d5c26f1316054115f637330f735c72e22480c
Gustavo Cotta <gmcotta34@gmail.com>
2023-01-05 21:52:08 -0300

    feat: ensure PrivateRoute renders page if token is not empty

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
-import { setCurrentAccountAdapter } from '@/main/adapters'
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '@/main/adapters'
 import { makeLogin as MakeLogin } from '@/main/factories/pages/login/login-factory'
 import { makeSignup as MakeSignup } from '@/main/factories/pages/signup/signup-factory'
 import { SurveyList } from '@/presentation/pages'
@@ -9,7 +9,10 @@ import { APIContext } from '@/presentation/contexts'
 
 const Router: FC = () => {
   return (
-    <APIContext.Provider value={{ setCurrentAccount: setCurrentAccountAdapter }}>
+    <APIContext.Provider value={{
+      setCurrentAccount: setCurrentAccountAdapter,
+      getCurrentAccount: getCurrentAccountAdapter
+    }}>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<MakeLogin />} />
--- src/presentation/components/private-route/private-route.tsx
+++ src/presentation/components/private-route/private-route.tsx
@@ -1,8 +1,15 @@
-import React, { FC } from 'react'
+import React, { FC, ReactElement, useContext } from 'react'
 import { Navigate } from 'react-router-dom'
 
-const PrivateRoute: FC = () => {
-  return <Navigate to="/login" />
+import { APIContext } from '@/presentation/contexts'
+
+type PrivateRouteProps = {
+  children: ReactElement
+}
+
+const PrivateRoute: FC<PrivateRouteProps> = ({ children }) => {
+  const { getCurrentAccount } = useContext(APIContext)
+  return getCurrentAccount()?.accessToken ? children : <Navigate to="/login" />
 }
 
 export default PrivateRoute
