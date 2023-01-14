73a55cf44cb547c1fda099ad438ae46ff2309422
Gustavo Cotta <gmcotta34@gmail.com>
2023-01-13 22:20:08 -0300

    feat: add AuthorizeHttpGetClientDecorator factory

--- src/main/factories/usecases/load-survey-list/remote-load-survey-list.factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list.factory.ts
@@ -1,7 +1,8 @@
 import { RemoteLoadSurveyList } from '@/data/usecases/load-survey-list/remote-load-survey-list'
 import { LoadSurveyList } from '@/domain/usecases'
-import { makeAPIUrl, makeAxiosHttpClient } from '@/main/factories/http'
+import { makeAuthorizeHttpGetClientDecorator } from '@/main/factories/decorators'
+import { makeAPIUrl } from '@/main/factories/http'
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
-  return new RemoteLoadSurveyList(makeAPIUrl('/surveys'), makeAxiosHttpClient())
+  return new RemoteLoadSurveyList(makeAPIUrl('/surveys'), makeAuthorizeHttpGetClientDecorator())
 }
