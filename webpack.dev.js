a770a7b89a33331089ae20359e205ac72060c13f
Gustavo Cotta <gmcotta34@gmail.com>
2023-01-17 20:47:50 -0300

    chore: add webpack's config for dev and prod envs

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,22 +1,10 @@
-
-const path = require('path')
 const { DefinePlugin } = require('webpack')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const common = require('./webpack.common')
+const { merge } = require('webpack-merge')
 
-module.exports = {
+module.exports = merge(common, {
   mode: 'development',
-  entry: './src/main/index.tsx',
-  output: {
-    path: path.join(__dirname, 'public/js'),
-    publicPath: '/public/js',
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'scss'],
-    alias: {
-      '@': path.join(__dirname, 'src')
-    }
-  },
   module: {
     rules: [
       {
@@ -44,7 +32,7 @@ module.exports = {
     ]
   },
   devServer: {
-    static: './public',
+    static: './dist',
     devMiddleware: {
       writeToDisk: true
     },
@@ -57,14 +45,12 @@ module.exports = {
       ]
     }
   },
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
-  },
   plugins: [
-    new CleanWebpackPlugin(),
     new DefinePlugin({
       'process.env.API_URL': JSON.stringify('http://localhost:5050/api')
+    }),
+    new HtmlWebpackPlugin({
+      template: './template.dev.html'
     })
   ]
-}
+})
