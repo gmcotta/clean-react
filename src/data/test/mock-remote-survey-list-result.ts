19d18d2abe6995ce2ef89dc2931477b45d62c425
Gustavo Cotta <gmcotta34@gmail.com>
2023-01-31 21:25:02 -0300

    test: ensure RemoteLoadSurveyResult returns SurveyResultModel on 200

---
+++ src/data/test/mock-remote-survey-list-result.ts
@@ -0,0 +1,20 @@
+import { faker } from '@faker-js/faker'
+
+import { RemoteLoadSurveyListResult } from '@/data/usecases'
+
+export const mockRemoteSurveyResultModel = (): RemoteLoadSurveyListResult.Model => ({
+  question: faker.random.words(10),
+  date: faker.date.recent().toISOString(),
+  answers: [
+    {
+      image: faker.image.abstract(),
+      answer: faker.random.words(),
+      count: Number(faker.random.numeric(2)),
+      percent: Number(faker.random.numeric(2))
+    },
+    {
+      answer: faker.random.words(),
+      count: Number(faker.random.numeric(2)),
+      percent: Number(faker.random.numeric(2))
+    }]
+})
--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker'
 
-import { HttpGetClientSpy } from '@/data/test'
+import { HttpGetClientSpy, mockRemoteSurveyResultModel } from '@/data/test'
 import { HttpStatusCode } from '@/data/protocols/http'
 import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 
@@ -25,6 +25,10 @@ describe('RemoteLoadSurveyResult', () => {
   it('Should call HttpGetClient with correct URL', async () => {
     const url = faker.internet.url()
     const { sut, httpGetClientSpy } = makeSut(url)
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: mockRemoteSurveyResultModel()
+    }
     await sut.load()
     expect(httpGetClientSpy.url).toBe(url)
   })
@@ -54,5 +58,22 @@ describe('RemoteLoadSurveyResult', () => {
     }
     const promise = sut.load()
     await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  it('Should return a SurveyReturn on 200', async () => {
+    const httpResult = mockRemoteSurveyResultModel()
+    const { sut, httpGetClientSpy } = makeSut()
+
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult
+    }
+    const httpResponse = await sut.load()
+    expect(httpResponse.answers).toHaveLength(2)
+    expect(httpResponse).toStrictEqual({
+      question: httpResult.question,
+      answers: httpResult.answers,
+      date: new Date(httpResult.date)
+    })
   })
 })
