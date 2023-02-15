fff30856bf8a7d9f506f34842fabbc97937c0404
Gustavo Cotta <gmcotta34@gmail.com>
2023-02-14 21:10:05 -0300

    feat: add skeleton shimmer effect on SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -10,28 +10,32 @@ const SurveyResult: FC = () => {
     <div className={Styles.surveyResultWrapper}>
       <Header />
       <div className={Styles.contentWrapper}>
-        <hgroup>
-          <Calendar date={new Date()} className={Styles.calendarWrapper} />
-          <h2>Qual é o seu framework web favorito?</h2>
-        </hgroup>
-        <FlipMove className={Styles.answerList}>
-          <li>
-            <img src="" alt="" />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-          <li className={Styles.active}>
-            <img src="" alt="" />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-          <li>
-            <img src="" alt="" />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-        </FlipMove>
-        <button>Voltar</button>
+        {false &&
+          <>
+            <hgroup>
+              <Calendar date={new Date()} className={Styles.calendarWrapper} />
+              <h2>Qual é o seu framework web favorito?</h2>
+            </hgroup>
+            <FlipMove className={Styles.answerList}>
+              <li>
+                <img src="" alt="" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+              <li className={Styles.active}>
+                <img src="" alt="" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+              <li>
+                <img src="" alt="" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+            </FlipMove>
+            <button>Voltar</button>
+          </>
+        }
         {false && <Loading />}
       </div>
       <Footer />
