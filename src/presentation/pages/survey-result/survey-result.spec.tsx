2a16b7d907754ac4192569c473f2d7ba0f75d251
Gustavo Cotta <gmcotta34@gmail.com>
2023-02-18 12:19:05 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -7,19 +7,24 @@ import { mockAccountModel } from '@/domain/test'
 import { APIContext } from '@/presentation/contexts'
 import SurveyResult from './survey-result'
 
+const makeSut = (): void => {
+  const history = createMemoryHistory({ initialEntries: ['/surveys'] })
+
+  render(
+    <APIContext.Provider value={{
+      setCurrentAccount: jest.fn(),
+      getCurrentAccount: () => mockAccountModel()
+    }}>
+      <Router location={history.location} navigator={history}>
+        <SurveyResult />|
+      </Router>
+    </APIContext.Provider>
+  )
+}
+
 describe('SurveyResult', () => {
   it('Should show correct initial stage', () => {
-    const history = createMemoryHistory({ initialEntries: ['/surveys'] })
-    render(
-      <APIContext.Provider value={{
-        setCurrentAccount: jest.fn(),
-        getCurrentAccount: () => mockAccountModel()
-      }}>
-        <Router location={history.location} navigator={history}>
-          <SurveyResult />|
-        </Router>
-      </APIContext.Provider>
-    )
+    makeSut()
     const surveyResult = screen.getByTestId('survey-result')
     expect(surveyResult.childElementCount).toBe(0)
     expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
